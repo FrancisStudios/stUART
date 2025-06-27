8ad0410665f0eb13f84dd5709fa1fecb22eaad72
Francis <vlfmap@gmail.com>
2025-06-27 17:30:42 +0200

    header guards

--- stuart.h
+++ stuart.h
@@ -1,6 +1,12 @@
 #ifndef STUART_H
 #define STUART_H
 
+#include <Arduino.h> //(un)comment if you are (not) using arduino 
+/** This library is currently written for arduino ecosystem primarily
+so delay and other arduino implementations must be ported manually to
+each environment -- will be included in this library in the future (pbbly)
+-- it might be a good way to contribute for anyone, just drop a PR xD :)
+*/
 
 
 #endif
