d591fe15e1e2246618619ac6a7298e73058a576d
Francis <vlfmap@gmail.com>
2025-06-28 10:37:10 +0200

    file signatures

--- util.h
+++ util.h
@@ -1,3 +1,10 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * .. github:https://github.com/francisstudios ..
+ * ____ ©2025 Francis Studios Softwares by L. ___
+ */
+
 #pragma once
 
 namespace stUTIL
